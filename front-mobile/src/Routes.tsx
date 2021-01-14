98603c5ed0633975e148f15d680ccf55fd0c82c7
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-01-14 21:17:42 +0000

    Finished Project

--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -2,7 +2,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./Home";
 import Orders from "./Orders.tsx";
+import OrdersDetails from "./OrderDetails.tsx";
+
 import React from 'react';
+import OrderDetails from "./OrderDetails";
 
 
 
@@ -21,6 +24,8 @@ function Routes() {
             >
                 <Stack.Screen name="Home" component={Home}></Stack.Screen>
                 <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+                <Stack.Screen name="OrderDetails" component={OrderDetails}></Stack.Screen>
+
             </Stack.Navigator>
         </NavigationContainer>
     )
